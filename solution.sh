53122ccbca362469f4a5742b0c7ebdf6a8396ce6
Siddanth Palani <massmaranam.2007@gmail.com>
2025-08-28 14:55:07 +0530

    Update solution.sh

--- solution.sh
+++ solution.sh
@@ -12,7 +12,7 @@ cat readme.txt
 wc -l data.csv
 
 # 5. Find all files containing the word "error" in the "logs" folder
-grep -il error logs
+grep -rl error logs
 
 # 6. Display the last 10 lines of "app.log"
 tail -n10 app.log
